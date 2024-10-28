ce63dba0617c7a3ae749196db079fd9ea7066f49
clem-ant <clementfourmii@hotmail.fr>
2024-10-28 17:17:03 +0100

    svg green circle

--- app/homeUI/userCount.jsx
+++ app/homeUI/userCount.jsx
@@ -11,7 +11,17 @@ export default function UserCount() {
     socket.emit("getClientCount");
   }, []);
   return (
-    <div className="text-white bg-primary  p-3">
+    <div className="text-white bg-primary p-3 rounded-full flex items-center">
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="green"
+        xmlns="http://www.w3.org/2000/svg"
+        className="mr-2"
+      >
+        <circle cx="5" cy="5" r="5" />
+      </svg>
       {userCount} utilisateur{userCount > 1 ? "s" : ""} en ligne
     </div>
   );
